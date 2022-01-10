95b6a5ce5fb9393f6f564a7f0d1305784de7081a
Kathrin Sánchez <kathesanchezguzman@gmail.com>
2022-01-10 13:29:59 -0500

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template: `
+   //<h1>Hola de Nuevo</h1>`
+  
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+
+}
